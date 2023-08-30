d49b661f25dc091d41b273577d19ed00f0221343
Michael Horojanski <mic3ael@gmail.com>
2023-08-30 12:39:04 +0300

    project services refactoring

--- main.tf
+++ main.tf
@@ -1,17 +1,18 @@
 locals {
-  env = terraform.workspace
+  env          = terraform.workspace
+  servicesPath = "services"
 }
 
 module "iac_handlers" {
   source = "./modules/iac-handlers"
   env    = local.env
   lambdas = [{
-    output_path   = "${path.module}/permutation/archive_files/function.zip",
+    output_path   = "${path.module}/${local.servicesPath}/permutation/archive_files/function.zip",
     function_name = "permutation",
     timeout       = 300,
     handler       = "src/index.handler"
     runtime       = "nodejs18.x"
-    source_dir    = "${path.module}/permutation/function"
+    source_dir    = "${path.module}/${local.servicesPath}/permutation/function"
   }]
 }
 
@@ -19,5 +20,5 @@ module "iac-gateway" {
   source  = "./modules/iac-gateway"
   env     = local.env
   count   = length(module.iac_handlers.lambda_functions)
-  targets = [{ lambda : module.iac_handlers.lambda_functions[count.index], endpoint : { route : "permutation", method : "POST", schema : file("${path.module}/permutation/schema.json") } }]
+  targets = [{ lambda : module.iac_handlers.lambda_functions[count.index], endpoint : { route : "permutation", method : "POST", schema : file("${path.module}/${local.servicesPath}/permutation/schema.json") } }]
 }
