95eb55ae1cc907343f17e70fc5b0ac781095f356
Michael <mic3ael@gmail.com>
2023-09-25 14:19:06 +0300

    Update permutation.js

--- services/permutation/function/src/services/permutation.js
+++ services/permutation/function/src/services/permutation.js
@@ -5,7 +5,6 @@ function swap(array, i, j) {
 }
 
 function getPermutationsHelper(i, array, perms) {
-  // Write your code here.
   if (array.length - 1 === i) return perms.push(Array.from(array));
 
   for (let j = i; j < array.length; j++) {
